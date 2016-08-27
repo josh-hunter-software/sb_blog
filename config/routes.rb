5f5d85bb5cfe6eb6d24f7772a683c1eeed3e3eaf
Josh Hunter <thatsmegettinmail@yahoo.com>
2016-08-27 13:13:03 -0500

    shortened routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,63 +1,33 @@
 Rails.application.routes.draw do
   root to: "blogs#index"
 
-  resources :blogs do 
-    resources :posts do 
-      resources :comments 
-    end
-  end
+  get '/blogs(.:format)', to: "blogs#index", as: :blogs
+  post '/blogs(.:format)', to: "blogs#create"
+  get   '/blogs/new(.:format)', to: "blogs#new", as: :new_blog
+  patch '/:id(.:format)', to: 'blogs#update', as: :edit_blog
+  get '/:id(.:format)', to: "blogs#show", as: :blog
+  patch '/:id(.:format)', to: 'blogs#update'
+  put    '/:id(.:format)', to: 'blogs#update'
+  delete '/:id(.:format)', to: 'blogs#destroy'
+
+  get   '/:blog_id/posts(.:format)', to: "posts#index", as: :blog_posts  
+  post  '/:blog_id/posts(.:format)', to: "posts#create"
+  get   '/:blog_id/new(.:format)', to: 'posts#new', as: :new_blog_post
+  get   '/:blog_id/:id/edit(.:format)', to: 'posts#edit', as: :edit_blog_post
+  get   '/:blog_id/:id(.:format)', to: 'posts#show', as: :blog_post
+  patch '/:blog_id/:id(.:format)', to: 'posts#update'
+  put   '/:blog_id/:id(.:format)', to: 'posts#update'
+  delete '/:blog_id/:id(.:format)', to: 'posts#destroy'
+
+  get '/:blog_id/:post_id/comments(.:format)', to: 'comments#index', as: :blog_post_comments
+  post '/:blog_id/:post_id/comments(.:format)', to: 'comments#create'
+  get '/:blog_id/:post_id/comments/new(.:format)', to: 'comments#new', as: :new_blog_post_comment
+  get '/:blog_id/:post_id/comments/:id/edit(.:format)', to: 'comments#edit', as: :edit_blog_post_comment
+  get '/:blog_id/:post_id/comments/:id(.:format)', to: 'comments#show', as: :blog_post_comment 
+  patch  '/:blog_id/:post_id/comments/:id(.:format)', to: 'comments#update'
+  put    '/:blog_id/:post_id/comments/:id(.:format)', to: 'comments#update'
+  delete '/:blog_id/:post_id/comments/:id(.:format)', to: 'comments#destroy'
+
   devise_for :users
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
